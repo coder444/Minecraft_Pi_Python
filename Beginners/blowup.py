574306b041fc25b172bd7cc4df7b199445aa3597
coder444 <wz68.scratch@gmail.com>
2015-10-02 14:46:40 -0700

    Update blowup.py

--- Beginners/blowup.py
+++ Beginners/blowup.py
@@ -2,3 +2,10 @@ from mcpi import minecraft
 mc = minecraft.Minecraft.create()
 
 while True:
+  #Sets a XYZ Position to where you Right Click a block
+  e = mc.events.pollBlockHits()
+  #Goes through variable e
+  for ev in e:
+    #If the ID that pollBlockHits collects is 46 (TNT), it places a ready to explode TNT
+    if(mc.getBlockWithData(ev.pos).id == 46):
+      mc.setBlock(ev.pos, 46, 1)
