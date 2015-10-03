38821ac8ddad072bde54580a9b66808007404997
coder444 <wz68.scratch@gmail.com>
2015-10-02 18:00:56 -0700

    Update camera.py

--- Beginners/camera.py
+++ Beginners/camera.py
@@ -1,2 +1,12 @@
 from mcpi import minecraft
 mc = minecraft.Minecraft.create()
+
+mc.postToChat('Your camera has been FROZEN!')
+#Make the camera fixed
+mc.camera.setFixed()
+i = 10
+while i > 0:
+  mc.postToChat(i)
+mc.postToChat('Your camera has been set NORMAL!')
+#Reset the camera
+mc.camera.setNormal()
