a315fc70e6cf667bd7154573c40ab6ceef31b693
wz68.scratch <pi@raspberrypi.(none)>
2015-11-10 00:38:18 +0000

    MINING TURTLE

---
+++ turtle.py
@@ -0,0 +1,18 @@
+from mcpi import minecraft
+from time import sleep
+mc = minecraft.Minecraft.create()
+
+while True:
+    e = mc.events.pollBlockHits()
+    for ev in e:
+        x, y, z = ev.pos
+        while True:
+            mc.setBlock(x, y + 1, z, 44)
+            mc.setBlock(x, y, z, 89)
+            sleep(0.1)
+            mc.setBlock(x, y, z, 0)
+            mc.setBlock(x, y + 1, z, 0)
+            x += 1
+            mc.setBlocks(x, y, z, x + 5, y + 5, z + 3, 0)
+            x += 1
+            sleep(0.5)
